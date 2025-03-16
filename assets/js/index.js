3e45d94376b41da136eed336aa8400689e17c281
Panther291 <thepanther291@gmail.com>
2025-03-16 11:48:25 +0530

    .

--- assets/js/index.js
+++
@@ -1,6 +0,0 @@
-const hamMenu = document.querySelector('#ham-menu')
-const offScreen = document.querySelector('#off-screen-menu')
-hamMenu.addEventListener('click', () => {
-    hamMenu.classList.toggle('ham-menu-active')
-    offScreen.classList.toggle('off-screen-menu-active')
-})
